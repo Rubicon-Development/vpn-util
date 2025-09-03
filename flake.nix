5c01ae1f14eaf48dd2cb28ef74c2add95ee01c1d
Liam Berry <liamandberry@gmail.com>
2025-09-03 11:56:29 +0200

    nix generate c file

--- flake.nix
+++ flake.nix
@@ -13,33 +13,61 @@
         lib = pkgs.lib;
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "vpn";
-          version = "0.1.0";
-          src = ./.;
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "vpn";
+            version = "0.1.0";
+            src = ./.;
 
-          # jpm comes with janet in nixpkgs
-          nativeBuildInputs = [ pkgs.jpm pkgs.janet ];
-          buildInputs = [ pkgs.jpm pkgs.janet ];
+            # jpm comes with janet in nixpkgs
+            nativeBuildInputs = [ pkgs.jpm pkgs.janet ];
+            buildInputs = [ pkgs.jpm pkgs.janet ];
 
-          # jpm may invoke ld directly; avoid GCC-style -Wl flags.
-          # We rely on wrapping to provide runtime lib path instead of rpath.
+            # jpm may invoke ld directly; avoid GCC-style -Wl flags.
+            # We rely on wrapping to provide runtime lib path instead of rpath.
 
-          buildPhase = ''
-            runHook preBuild
-            jpm build --libpath=${pkgs.janet}/lib
-            runHook postBuild
-          '';
+            buildPhase = ''
+              runHook preBuild
+              jpm build --libpath=${pkgs.janet}/lib
+              runHook postBuild
+            '';
 
-          installPhase = ''
-            runHook preInstall
-            install -Dm755 build/vpn $out/bin/vpn
-            runHook postInstall
-          '';
+            installPhase = ''
+              runHook preInstall
+              install -Dm755 build/vpn $out/bin/vpn
+              runHook postInstall
+            '';
 
-          meta = {
-            description = "vpn util built with Janet (jpm quickbin)";
-            platforms = lib.platforms.unix;
+            meta = {
+              description = "vpn util built with Janet (jpm quickbin)";
+              platforms = lib.platforms.unix;
+            };
+          };
+
+          vpn-c = pkgs.stdenv.mkDerivation {
+            pname = "vpn-c";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.jpm pkgs.janet ];
+            buildInputs = [ pkgs.jpm pkgs.janet ];
+
+            buildPhase = ''
+              runHook preBuild
+              jpm build --libpath=${pkgs.janet}/lib
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              install -Dm644 build/vpn.c $out/vpn.c
+              runHook postInstall
+            '';
+
+            meta = {
+              description = "Generated C source for vpn util";
+              platforms = lib.platforms.unix;
+            };
           };
         };
 
