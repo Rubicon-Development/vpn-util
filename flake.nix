a35dc6bc5b385a42c46bf491122f0cb9fd43335e
Liam Berry <liamandberry@gmail.com>
2025-11-20 17:44:52 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    janet2nix.url = "github:alan-strohm/janet2nix";
+
+    janet-lsp-src = {
+      url = "github:Blue-Berry/janet-lsp.nix";
+      flake = false;
+    };
+
+    spork = {
+      url = "github:janet-lang/spork";
+      flake = false;
+    };
+
     janet-c = {
       url = "https://github.com/janet-lang/janet/releases/download/v1.39.1/janet.c";
       flake = false;
@@ -14,24 +26,42 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      janet-c,
-      janet-h,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = pkgs.lib;
-      in
-      {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        j2nLib = inputs.janet2nix.lib.${system};
+        janet-lsp = pkgs.callPackage inputs.janet-lsp-src {};
+        inherit (pkgs) mkShell;
+        name = "vpn";
+
+        spork = j2nLib.mkJanetPackage {
+          name = "spork";
+          src = inputs.spork;
+        };
+      in {
+        devShells = {
+          default = mkShell {
+            inputsFrom = [self'.packages.default];
+            buildInputs = with pkgs; [
+              janet
+              jpm
+              janet-lsp
+            ];
+          };
+        };
+
         packages = {
           default = pkgs.stdenv.mkDerivation {
-            pname = "vpn";
+            pname = name;
             version = "0.1.3";
             src = ./.;
 
@@ -39,6 +69,7 @@
               pkgs.jpm
               pkgs.janet
             ];
+
             buildInputs = [
               pkgs.jpm
               pkgs.janet
@@ -46,6 +77,7 @@
 
             buildPhase = ''
               runHook preBuild
+              export JANET_PATH="${spork}/jpm_tree/lib"
               jpm build --libpath=${pkgs.janet}/lib
               runHook postBuild
             '';
@@ -58,7 +90,7 @@
 
             meta = {
               description = "vpn util built with Janet (jpm quickbin)";
-              platforms = lib.platforms.unix;
+              platforms = pkgs.lib.platforms.unix;
             };
           };
 
@@ -85,28 +117,24 @@
             installPhase = ''
               runHook preInstall
               install -Dm644 build/vpn.c $out/vpn.c
-              install -Dm644 ${janet-c} $out/janet.c
-              install -Dm644 ${janet-h} $out/janet.h
+              install -Dm644 ${inputs.janet-c} $out/janet.c
+              install -Dm644 ${inputs.janet-h} $out/janet.h
               runHook postInstall
             '';
 
             meta = {
               description = "Generated C source for vpn util";
-              platforms = lib.platforms.unix;
+              platforms = pkgs.lib.platforms.unix;
             };
           };
         };
 
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/vpn";
+        apps = {
+          default = {
+            type = "app";
+            program = "${self'.packages.default}/bin/vpn";
+          };
         };
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.janet
-          ];
-        };
-      }
-    );
+      };
+    };
 }
