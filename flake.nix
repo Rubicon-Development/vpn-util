6fde2c61e146c856863cfd4a252c5b8d33d97756
Liam Berry <liamandberry@gmail.com>
2026-05-27 11:54:53 +0200

    update

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     spork = {
-      url = "github:janet-lang/spork";
+      url = "github:janet-lang/spork/v1.1.1";
       flake = false;
     };
 
@@ -27,207 +27,239 @@
     };
 
     janet-c = {
-      url = "https://github.com/janet-lang/janet/releases/download/v1.40.1/janet.c";
+      url = "https://github.com/janet-lang/janet/releases/download/v1.41.2/janet.c";
       flake = false;
     };
     janet-h = {
-      url = "https://github.com/janet-lang/janet/releases/download/v1.40.1/janet.h";
+      url = "https://github.com/janet-lang/janet/releases/download/v1.41.2/janet.h";
       flake = false;
     };
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        j2nLib = inputs.janet2nix.lib.${system};
-        janet-lsp = pkgs.callPackage inputs.janet-lsp-src {};
-        inherit (pkgs) mkShell;
-        name = "vpn";
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          j2nLib = inputs.janet2nix.lib.${system};
+          janet-lsp = pkgs.callPackage inputs.janet-lsp-src { };
+          inherit (pkgs) mkShell;
+          name = "vpn";
 
-        spork = j2nLib.mkJanetPackage {
-          name = "spork";
-          src = inputs.spork;
-        };
-
-        jdoc = j2nLib.mkJanetPackage {
-          name = "jdoc";
-          src = inputs.jdoc;
-        };
-
-        jurl = pkgs.stdenv.mkDerivation {
-          pname = "jurl";
-          version = "1.4.3";
-          src = inputs.jurl;
-
-          nativeBuildInputs = [pkgs.jpm pkgs.janet];
-          buildInputs = [pkgs.curl];
-
-          buildPhase = ''
-            runHook preBuild
-            jpm build
-            runHook postBuild
-          '';
-
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out/jpm_tree
-            export JANET_TREE=$out/jpm_tree
-            jpm install
-            runHook postInstall
-          '';
-        };
-      in {
-        devShells = {
-          default = mkShell {
-            inputsFrom = [self'.packages.default];
-            buildInputs = with pkgs; [
-              janet
-              jpm
-              janet-lsp
-            ];
-            shellHook = ''
-              export JANET_PATH="${jdoc}/jpm_tree/lib:$JANET_PATH"
-              export PATH="${jdoc}/jpm_tree/bin:$PATH"
-            '';
-          };
-        };
-
-        packages = {
-          default = pkgs.stdenv.mkDerivation {
-            pname = name;
-            version = "0.1.6";
-            src = ./.;
+          spork = pkgs.stdenv.mkDerivation {
+            pname = "spork";
+            version = "1.1.1";
+            src = inputs.spork;
 
             nativeBuildInputs = [
               pkgs.jpm
               pkgs.janet
             ];
 
-            buildInputs = [
-              pkgs.jpm
-              pkgs.janet
-              pkgs.curl
-            ];
-
             buildPhase = ''
               runHook preBuild
-              # Create a combined module directory
-              mkdir -p combined_modules
-              cp -r ${spork}/jpm_tree/lib/* combined_modules/ || true
-              cp -r ${jurl}/jpm_tree/lib/* combined_modules/ || true
-              export JANET_PATH="$(pwd)/combined_modules"
-              jpm build --libpath=${pkgs.janet}/lib --modpath=$(pwd)/combined_modules
+              jpm build
               runHook postBuild
             '';
 
             installPhase = ''
               runHook preInstall
-              install -Dm755 build/vpn $out/bin/vpn
-              install -Dm644 completions/vpn.bash $out/share/bash-completion/completions/vpn
-              install -Dm644 completions/_vpn $out/share/zsh/site-functions/_vpn
+              mkdir -p $out/jpm_tree
+              export JANET_TREE=$out/jpm_tree
+              jpm install
               runHook postInstall
             '';
-
-            meta = {
-              description = "vpn util built with Janet (jpm quickbin)";
-              platforms = pkgs.lib.platforms.unix;
-            };
           };
 
-          vpn-c = pkgs.stdenv.mkDerivation {
-            pname = "vpn-c";
-            version = "0.1.4";
-            src = ./.;
+          jdoc = j2nLib.mkJanetPackage {
+            name = "jdoc";
+            src = inputs.jdoc;
+          };
+
+          jurl = pkgs.stdenv.mkDerivation {
+            pname = "jurl";
+            version = "1.4.3";
+            src = inputs.jurl;
 
             nativeBuildInputs = [
               pkgs.jpm
               pkgs.janet
             ];
-            buildInputs = [
-              pkgs.jpm
-              pkgs.janet
-              pkgs.curl
-            ];
+            buildInputs = [ pkgs.curl ];
 
             buildPhase = ''
               runHook preBuild
-              # Create a combined module directory
-              mkdir -p combined_modules
-              cp -r ${spork}/jpm_tree/lib/* combined_modules/ || true
-              cp -r ${jurl}/jpm_tree/lib/* combined_modules/ || true
-              export JANET_PATH="$(pwd)/combined_modules"
-              jpm build --libpath=${pkgs.janet}/lib --modpath=$(pwd)/combined_modules
+              jpm build
               runHook postBuild
             '';
 
             installPhase = ''
               runHook preInstall
-              install -Dm644 build/vpn.c $out/vpn.c
-              install -Dm644 ${inputs.janet-c} $out/janet.c
-              install -Dm644 ${inputs.janet-h} $out/janet.h
+              mkdir -p $out/jpm_tree
+              export JANET_TREE=$out/jpm_tree
+              jpm install
               runHook postInstall
             '';
-
-            meta = {
-              description = "Generated C source for vpn util";
-              platforms = pkgs.lib.platforms.unix;
+          };
+        in
+        {
+          devShells = {
+            default = mkShell {
+              inputsFrom = [ self'.packages.default ];
+              buildInputs = with pkgs; [
+                janet
+                jpm
+                janet-lsp
+              ];
+              shellHook = ''
+                export JANET_PATH="${jdoc}/jpm_tree/lib:$JANET_PATH"
+                export PATH="${jdoc}/jpm_tree/bin:$PATH"
+              '';
             };
           };
 
-          vpn-static = pkgs.stdenv.mkDerivation {
-            pname = "vpn-static";
-            version = "0.1.0";
-            src = ./.;
+          packages = {
+            default = pkgs.stdenv.mkDerivation {
+              pname = name;
+              version = "0.1.6";
+              src = ./.;
 
-            buildInputs = [
-              pkgs.curl
-            ];
+              nativeBuildInputs = [
+                pkgs.jpm
+                pkgs.janet
+              ];
 
-            unpackPhase = ''
-              cp ${self'.packages.vpn-c}/vpn.c vpn.c
-              cp ${self'.packages.vpn-c}/janet.c janet.c
-              cp ${self'.packages.vpn-c}/janet.h janet.h
-              cp ${spork}/jpm_tree/lib/spork/json.a spork-json.a
-              cp ${jurl}/jpm_tree/lib/jurl/native.a jurl-native.a
-              cp -r $src/completions completions
-            '';
+              buildInputs = [
+                pkgs.jpm
+                pkgs.janet
+                pkgs.curl
+              ];
 
-            buildPhase = ''
-              runHook preBuild
-              $CC -o vpn vpn.c janet.c spork-json.a jurl-native.a -I. -lm -ldl -lcurl -O2
-              runHook postBuild
-            '';
+              buildPhase = ''
+                runHook preBuild
+                # Create a combined module directory
+                mkdir -p combined_modules
+                cp -r ${spork}/jpm_tree/lib/* combined_modules/ || true
+                cp -r ${jurl}/jpm_tree/lib/* combined_modules/ || true
+                export JANET_PATH="$(pwd)/combined_modules"
+                jpm build --libpath=${pkgs.janet}/lib --modpath=$(pwd)/combined_modules
+                runHook postBuild
+              '';
 
-            installPhase = ''
-              runHook preInstall
-              install -Dm755 vpn $out/bin/vpn
-              install -Dm644 completions/vpn.bash $out/share/bash-completion/completions/vpn
-              install -Dm644 completions/_vpn $out/share/zsh/site-functions/_vpn
-              runHook postInstall
-            '';
+              installPhase = ''
+                runHook preInstall
+                install -Dm755 build/vpn $out/bin/vpn
+                install -Dm644 completions/vpn.bash $out/share/bash-completion/completions/vpn
+                install -Dm644 completions/_vpn $out/share/zsh/site-functions/_vpn
+                runHook postInstall
+              '';
 
-            meta = {
-              description = "VPN util compiled from C source";
-              platforms = pkgs.lib.platforms.unix;
+              meta = {
+                description = "vpn util built with Janet (jpm quickbin)";
+                platforms = pkgs.lib.platforms.unix;
+              };
+            };
+
+            vpn-c = pkgs.stdenv.mkDerivation {
+              pname = "vpn-c";
+              version = "0.1.4";
+              src = ./.;
+
+              nativeBuildInputs = [
+                pkgs.jpm
+                pkgs.janet
+              ];
+              buildInputs = [
+                pkgs.jpm
+                pkgs.janet
+                pkgs.curl
+              ];
+
+              buildPhase = ''
+                runHook preBuild
+                # Create a combined module directory
+                mkdir -p combined_modules
+                cp -r ${spork}/jpm_tree/lib/* combined_modules/ || true
+                cp -r ${jurl}/jpm_tree/lib/* combined_modules/ || true
+                export JANET_PATH="$(pwd)/combined_modules"
+                jpm build --libpath=${pkgs.janet}/lib --modpath=$(pwd)/combined_modules
+                runHook postBuild
+              '';
+
+              installPhase = ''
+                runHook preInstall
+                install -Dm644 build/vpn.c $out/vpn.c
+                install -Dm644 ${inputs.janet-c} $out/janet.c
+                install -Dm644 ${inputs.janet-h} $out/janet.h
+                runHook postInstall
+              '';
+
+              meta = {
+                description = "Generated C source for vpn util";
+                platforms = pkgs.lib.platforms.unix;
+              };
+            };
+
+            vpn-static = pkgs.stdenv.mkDerivation {
+              pname = "vpn-static";
+              version = "0.1.0";
+              src = ./.;
+
+              buildInputs = [
+                pkgs.curl
+              ];
+
+              unpackPhase = ''
+                cp ${self'.packages.vpn-c}/vpn.c vpn.c
+                cp ${self'.packages.vpn-c}/janet.c janet.c
+                cp ${self'.packages.vpn-c}/janet.h janet.h
+                cp ${spork}/jpm_tree/lib/spork/json.a spork-json.a
+                cp ${jurl}/jpm_tree/lib/jurl/native.a jurl-native.a
+                cp -r $src/completions completions
+              '';
+
+              buildPhase = ''
+                runHook preBuild
+                $CC -o vpn vpn.c janet.c spork-json.a jurl-native.a -I. -lm -ldl -lcurl -O2
+                runHook postBuild
+              '';
+
+              installPhase = ''
+                runHook preInstall
+                install -Dm755 vpn $out/bin/vpn
+                install -Dm644 completions/vpn.bash $out/share/bash-completion/completions/vpn
+                install -Dm644 completions/_vpn $out/share/zsh/site-functions/_vpn
+                runHook postInstall
+              '';
+
+              meta = {
+                description = "VPN util compiled from C source";
+                platforms = pkgs.lib.platforms.unix;
+              };
+            };
+          };
+
+          apps = {
+            default = {
+              type = "app";
+              program = "${self'.packages.default}/bin/vpn";
             };
           };
         };
-
-        apps = {
-          default = {
-            type = "app";
-            program = "${self'.packages.default}/bin/vpn";
-          };
-        };
-      };
     };
 }
--- project.janet
+++ project.janet
@@ -2,8 +2,7 @@
   :name "vpn-util"
   :description "VPN utility for hostname resolution and connection"
   :dependencies ["spork"
-                 {:repo "https://github.com/cosmictoast/jurl.git"
-                  :tag "v1.4.3"}])
+                 {:repo "https://github.com/cosmictoast/jurl.git"}])
 
 (declare-executable
   :name "vpn"
